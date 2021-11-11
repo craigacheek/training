90b9708aca9e55aed7d5c0734fb2b7865593f7fc
craigacheek <71669999+craigacheek@users.noreply.github.com>
2021-11-11 15:31:15 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,8 @@
 userText = "hollow knight cat dog" 
 userText1 = "cod"
 
-userNumber = 69
-userNumber1 = 420
+userNumber = 0
+userNumber1 = 0
 
 userSum = userNumber + userNumber1 
 print(userSum)
@@ -10,7 +10,10 @@ print(userSum)
 print(userText , " " , userText1)
 
 
-
+userNumber = int(input("what is your first number?"))
+userNumber1 = int(input("what is your second number?"))
+print("your numbers are:", userNumber, "&" ,userNumber1)
+print("The product of your numbers is", userNumber*userNumber1)
 
 
 
