d530777d12069219210997e3737385c329f5a7c4
bandit0978 <94090093+bandit0978@users.noreply.github.com>
2021-11-10 19:18:14 -0800

    Made our first project

---
+++ helloworld.py
@@ -0,0 +1,29 @@
+userText = "hollow knight cat dog" 
+userText1 = "cod"
+
+userNumber = 69
+userNumber1 = 420
+
+userSum = userNumber + userNumber1 
+print(userSum)
+
+print(userText , " " , userText1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
